e6501fae3694fad8c72c9df755d0f5eeb6758fcf
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2024-12-30 08:08:27 +0530

    initial commit with 2 files

---
+++ basic/circle.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var msg string = "The value of Pi is:"
+	const Pi float64 = 3.14159
+	radius :=5.0
+	circumference := 2 * pi * radius
+	fmt.Println(msg, circumference)
+
+}
---
+++ basic/first.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+var name:string = 'Alice',
+age:=30
+var isActive bool = true
+
+// constants
+const pi = 3.14159
+const (
+	StatusOK = 200
+	StatusNotFound=404
+)
+
+//type conversions
+var a int = 10
+var b float64 = float64(a)
+var u uint = uint(f)
+
